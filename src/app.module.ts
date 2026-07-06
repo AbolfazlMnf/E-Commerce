f2b214b0cb37a05af95bacff7854b99f610b7b4a
Abolfazl <abolfazlmnf83@gmail.com>
2026-07-06 15:24:54 +0330

    time middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BlogModule } from './blog/blog.module';
 import { BlogCategoryModule } from './blog-category/blog-category.module';
+import { TimeMiddleware } from './shared/middlewares/time.middleware';
 
 @Module({
   imports: [
@@ -19,4 +20,8 @@ import { BlogCategoryModule } from './blog-category/blog-category.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(TimeMiddleware).forRoutes(`*`);
+  }
+}
